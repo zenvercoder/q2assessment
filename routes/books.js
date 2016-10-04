68f5b98eae64c882c769c1e1a4f3b4add3b3b49d
Lisa Hull <lisa@jchull.com>
2016-10-04 15:13:46 -0600

    get_book route, view with good edit/delete links

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 var databaseConnection = require("../database_connection");
 
-/* GET users listing. */
+router.get('/:id', function (req, res, next) {
+    databaseConnection("book").select().where("id", req.params.id)
+        .then(function (books) {
+        res.render("get_book", {books: books[0]});
+    });
+});
+
 router.get('/', function (req, res, next) {
     databaseConnection("book").select().then(function (books) {
         res.render("list_books", {"books": books});
