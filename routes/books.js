1f65c31ba747753a84de8c9ce29ba56a7c36f90a
Lisa Hull <lisa@jchull.com>
2016-10-05 12:46:01 -0600

    get a single book working again

--- routes/books.js
+++ routes/books.js
@@ -3,26 +3,16 @@ var router = express.Router();
 var databaseConnection = require("../database_connection");
 
 router.get('/:id', function (req, res, next) {
-    databaseConnection("book").select()
-        .innerJoin("book_author", "book.id", "book_id")
-        .innerJoin("author", "author_id", "author.id")
-        .where("book.id", req.params.id)
+    databaseConnection("book").select().where("id", req.params.id)
         .then(function (books) {
-            var books = mapAuthorsToBooks(books);
-            console.log(books[0]);
-            res.render("books/get_book", {layout: "books_layout", books: books[0]});
-        });
+        res.render("books/list_books", {"books": books});
+    });
 });
 
 router.get('/', function (req, res, next) {
-    databaseConnection("book")
-        .select()
-        .innerJoin("book_author", "book.id", "book_id")
-        .innerJoin("author", "author_id", "author.id")
-        .then(function (records) {
-            var books = mapAuthorsToBooks(records);
-            res.render("books/list_books", {layout: "books_layout", books: books});
-        });
+    databaseConnection("book").select().then(function (books) {
+        res.render("books/list_books", {"books": books});
+    });
 });
 
 router.get('/new', function (req, res, next) {
@@ -54,35 +44,22 @@ router.post('/', function (req, res, next) {
 router.get('/delete/:id', function (req, res, next) {
     databaseConnection("book").select().where("id", req.params.id)
         .then(function (books) {
-            res.render("books/delete_book", {layout: "books_layout", book: books[0]});
-        });
+        res.render("books/delete_book", {book: books[0]});
+    });
 });
 
-router.post("/:book_id/authors", function (req, res, next) {
-    console.log("post /:book_id/authors");
-    databaseConnection("book_author").insert({
-        book_id: parseInt(req.body.author_id),
-        author_id: parseInt(req.params.book_id)
-    }).then(function () {
+router.delete("/:id", function(req, res, next) {
+    console.log("got to delete");
+    databaseConnection("book").del().where("id", req.params.id)
+        .then(function () {
         res.redirect("/books");
     });
 });
 
-router.delete("/:book_id/authors/:author_id", function (req, res, next) {
-    console.log("delete /:book_id/authors/:author_id");
-    databaseConnection("book_author").del()
-        .where({
-            book_id: parseInt(req.params.book_id),
-            author_id: parseInt(req.params.author_id)
-        }).then(function () {
-        res.redirect("/books");
-    });
-});
-
-router.get("/edit/:id", function (req, res, next) {
+router.get("/edit/:id", function (req, res, next){
     databaseConnection("book").select().where("id", req.params.id)
         .then(function (books) {
-            res.render("books/edit_book", {layout: "books_layout", book: books[0]});
+            res.render("books/edit_book", {book: books[0]});
         });
 });
 
@@ -104,8 +81,8 @@ router.put('/:id', function (req, res, next) {
             cover_url: req.body.cover_image_url
         }).where("id", req.params.id)
             .then(function (books) {
-                res.redirect("/books");
-            });
+            res.redirect("/books");
+        });
     }
 });
 
